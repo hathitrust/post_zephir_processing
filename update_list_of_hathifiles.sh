0618a4bf84f7bdff28800d965b8f3f213fdcaa94
Josh Steverman <jstever@umich.edu>
2022-04-20 14:39:10 -0400

    Prevent cp of generated htfiles to archive or web

--- update_list_of_hathifiles.sh
+++
@@ -1,8 +0,0 @@
-# Generate the list of hathifiles for the website. 
-# The daily scripts do this automatically, but very occasionally they need a 
-# manual clean up. This is so we don't need to hunt down the appropriate script
-# every time.
-# run with: 
-# sudo -u libadm
-eval "$(perl -I/l1/govdocs/zcode/local/lib/perl5 -Mlocal::lib=/l1/govdocs/zcode/local)"
-`/htapps/www/sites/www.hathitrust.org/extra_perl/json_filelist.pl >> report.tmp`
