97dabc956cf61feefbcfa82e4687c8e747fc1830
Martin Warin <mwarin@umich.edu>
2024-12-09 15:29:26 -0500

    DEV-1415: method, fixture & test for json listing

--- lib/verifier/hathifiles_listing_verifier.rb
+++ lib/verifier/hathifiles_listing_verifier.rb
@@ -2,6 +2,7 @@
 
 require_relative "../verifier"
 require_relative "../derivatives"
+require "json"
 
 module PostZephirProcessing
   class HathifilesListingVerifier < Verifier
@@ -40,6 +41,28 @@ module PostZephirProcessing
 
     def verify_listing(path:)
       verify_file(path: path)
+
+      filename = File.basename(path)
+      verify_file_in_json(filename: filename)
+    end
+
+    # Verify that the derivatives for the date are included in
+    # "#{ENV['WWW_DIR']}/hathi_file_list.json"
+    def verify_file_in_json(filename:)
+      json_path = "#{ENV['WWW_DIR']}/hathi_file_list.json"
+      listings = JSON.load_file(json_path)
+      matches = []
+
+      listings.each do |listing|
+        if listing['filename'] == filename
+          matches << listing
+          break
+        end
+      end
+
+      if matches.empty?
+        error(message: "Did not find a listing with filename: #{filename} in JSON (#{json_path})")
+      end
     end
   end
 end
--- spec/unit/hathifiles_listing_verifier_spec.rb
+++ spec/unit/hathifiles_listing_verifier_spec.rb
@@ -8,79 +8,94 @@ require "logger"
 
 module PostZephirProcessing
   RSpec.describe(HathifilesListingVerifier) do
+    let(:verifier) { described_class.new }
+
+    # Using secondday here as a representative for
+    # "any day of the month that's not the 1st"
+    # missingday does not have files or listings
+    firstday = Date.parse("2023-01-01")
+    secondday = Date.parse("2023-01-02")
+    missingday = Date.parse("2023-01-13")
+
+    firstday_ymd = firstday.strftime("%Y%m%d")
+    secondday_ymd = secondday.strftime("%Y%m%d")
+    missingday_ymd = missingday.strftime("%Y%m%d")
+
+    dir_path = ENV["WWW_DIR"]
+
+    before(:all) do
+      FileUtils.cp("/usr/src/app/spec/fixtures/hathi_file_list.json", dir_path)
+    end
+
     around(:each) do |example|
       with_test_environment { example.run }
     end
 
-    # Using midmonth here as a stand-in for "any day of the month that's not the 1st"
-    firstday = Date.parse("2023-01-01")
-    midmonth = Date.parse("2023-01-15")
-    firstday_ymd = firstday.strftime("%Y%m%d")
-    midmonth_ymd = midmonth.strftime("%Y%m%d")
-
     describe "#derivatives_for_date" do
-      it "expects one derivative midmonth" do
-        expect(described_class.new.derivatives_for_date(date: midmonth).size).to eq 1
+      it "expects two derivativess on firstday" do
+        expect(described_class.new.derivatives_for_date(date: firstday).size).to eq 2
       end
 
-      it "expects two derivativess on the first of the month" do
-        expect(described_class.new.derivatives_for_date(date: firstday).size).to eq 2
+      it "expects one derivative on secondday" do
+        expect(described_class.new.derivatives_for_date(date: secondday).size).to eq 1
       end
     end
 
     describe "#verify_hathifiles_listing" do
-      dir_path = ENV["WWW_DIR"]
       FileUtils.mkdir_p(dir_path)
 
-      it "finds an update file midmonth" do
-        update_file = File.join(dir_path, "hathi_upd_#{midmonth_ymd}.txt.gz")
-
-        FileUtils.mkdir_p(dir_path)
-        FileUtils.touch(update_file)
-
-        verifier = described_class.new
-        verifier.verify_hathifiles_listing(date: midmonth)
-        expect(verifier.errors).to be_empty
-      end
-
-      it "finds both update and full file on the first day of the month" do
+      it "finds update and full file on firstday" do
         update_file = File.join(dir_path, "hathi_upd_#{firstday_ymd}.txt.gz")
         full_file = File.join(dir_path, "hathi_full_#{firstday_ymd}.txt.gz")
 
         FileUtils.touch(update_file)
         FileUtils.touch(full_file)
 
-        verifier = described_class.new
         verifier.verify_hathifiles_listing(date: firstday)
         expect(verifier.errors).to be_empty
       end
 
-      it "produces one error if upd file is missing mid month" do
-        # Make sure file does not exist
-        update_file = File.join(dir_path, "hathi_upd_#{midmonth_ymd}.txt.gz")
-        if File.exist?(update_file)
-          FileUtils.rm(update_file)
-        end
+      it "finds just an update file on secondday" do
+        update_file = File.join(dir_path, "hathi_upd_#{secondday_ymd}.txt.gz")
 
-        verifier = described_class.new
-        verifier.verify_hathifiles_listing(date: midmonth)
-        expect(verifier.errors.size).to eq 1
+        FileUtils.mkdir_p(dir_path)
+        FileUtils.touch(update_file)
+
+        verifier.verify_hathifiles_listing(date: secondday)
+        expect(verifier.errors).to be_empty
       end
 
-      it "produces two errors if upd and full file are missing on the first day of the month" do
-        # Make sure files do not exist
-        update_file = File.join(dir_path, "hathi_upd_#{firstday_ymd}.txt.gz")
-        full_file = File.join(dir_path, "hathi_full_#{firstday_ymd}.txt.gz")
+      it "produces 1 error if upd file is missing midmonth" do
+        verifier.verify_hathifiles_listing(date: missingday)
+        expect(verifier.errors.size).to eq 2
+        expect(verifier.errors).to include(/Did not find a listing with filename: hathi_upd_#{missingday_ymd}/)
+        expect(verifier.errors.first).to include(/not found:.+_upd_#{missingday_ymd}/)
+      end
 
-        [update_file, full_file].each do |f|
+      it "produces 2 errors if upd and full file are missing on the first day of the month" do
+        # Need to remove the 2 files for the first to test
+        verifier.derivatives_for_date(date: firstday).each do |f|
           if File.exist?(f)
             FileUtils.rm(f)
           end
         end
-
-        verifier = described_class.new
         verifier.verify_hathifiles_listing(date: firstday)
         expect(verifier.errors.size).to eq 2
+        expect(verifier.errors.first).to include(/not found:.+_upd_#{firstday_ymd}/)
+        expect(verifier.errors.last).to include(/not found:.+_full_#{firstday_ymd}/)
+      end
+    end
+
+    describe "#verify_file_in_json" do
+      it "finds a matching listing" do
+        verifier.verify_file_in_json(filename: "hathi_full_#{firstday_ymd}.txt.gz")
+        verifier.verify_file_in_json(filename: "hathi_upd_#{firstday_ymd}.txt.gz")
+        verifier.verify_file_in_json(filename: "hathi_upd_#{secondday_ymd}.txt.gz")
+      end
+      it "produces 1 error when not finding a matching listing" do
+        verifier.verify_file_in_json(filename: "hathi_upd_#{missingday_ymd}.txt.gz")
+        expect(verifier.errors.size).to eq 1
+        expect(verifier.errors).to include(/Did not find a listing with filename: hathi_upd_#{missingday_ymd}/)
       end
     end
   end
