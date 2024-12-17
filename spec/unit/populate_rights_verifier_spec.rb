dba6ce4d5d65b150940cb21fe1ced5faa348127b
Aaron Elkiss <aelkiss@umich.edu>
2024-12-17 16:03:13 -0500

    Refactor rights verifier
    * use around block
    * truncate before & after each example
    * separate into contexts for with & without rights in db

--- spec/unit/populate_rights_verifier_spec.rb
+++ spec/unit/populate_rights_verifier_spec.rb
@@ -34,77 +34,74 @@ module PostZephirProcessing
     end
 
     # Temporarily add each `htid` to `rights_current` with resonable (and irrelevant) default values.
-    def with_fake_rights_entries(htids: test_rights)
-      split_htids = htids.map { |htid| htid.split(".", 2) }
-      Services[:database][:rights_current].where([:namespace, :id] => split_htids).delete
-      split_htids.each do |split_htid|
-        insert_fake_rights(namespace: split_htid[0], id: split_htid[1])
-      end
-      begin
-        yield
-      ensure
-        Services[:database][:rights_current].where([:namespace, :id] => split_htids).delete
-      end
-    end
 
-    describe "#run_for_date" do
-      context "monthly" do
-        date = Date.new(2024, 11, 30)
-        context "with HTID in the Rights Database" do
-          it "logs no `missing rights_current` error" do
-            with_fake_rights_entries do
-              with_fake_rights_file(date: date, full: true) do
-                verifier.run_for_date(date: date)
-                expect(verifier.errors).not_to include(/missing rights_current/)
-              end
-            end
+    context "with HTIDs in the rights database" do
+      around(:each) do |example|
+        Services[:database][:rights_current].truncate
+
+        split_htids = test_rights.map { |htid| htid.split(".", 2) }
+        split_htids.each do |split_htid|
+          insert_fake_rights(namespace: split_htid[0], id: split_htid[1])
+        end
+
+        example.run
+
+        Services[:database][:rights_current].truncate
+      end
+
+      describe "#run_for_date" do
+        it "logs no `missing rights_current` error for full file" do
+          date = Date.new(2024, 11, 30)
+          with_fake_rights_file(date: date, full: true) do
+            verifier.run_for_date(date: date)
+            expect(verifier.errors).not_to include(/missing rights_current/)
           end
         end
 
-        context "with HTID not in the Rights Database" do
-          it "logs `missing rights_current` error" do
-            with_fake_rights_file(date: date, full: true) do
-              verifier.run_for_date(date: date)
-              expect(verifier.errors).to include(/missing rights_current/)
-            end
+        it "logs no `missing rights_current` error for update file" do
+          date = Date.new(2024, 12, 2)
+          with_fake_rights_file(date: date) do
+            verifier.run_for_date(date: date)
+            expect(verifier.errors).not_to include(/missing rights_current/)
           end
         end
       end
 
-      context "daily" do
-        date = Date.new(2024, 12, 2)
-        context "with HTID in the Rights Database" do
-          it "logs no `missing rights_current` error" do
-            with_fake_rights_entries do
-              with_fake_rights_file(date: date) do
-                verifier.run_for_date(date: date)
-                expect(verifier.errors).not_to include(/missing rights_current/)
-              end
-            end
-          end
-        end
-
-        context "with HTID not in the Rights Database" do
-          it "logs `missing rights_current` error" do
-            with_fake_rights_file(date: date) do
-              verifier.run_for_date(date: date)
-              expect(verifier.errors).to include(/missing rights_current/)
-            end
-          end
-        end
-      end
-    end
-
-    describe "#verify_rights_file" do
-      context "with HTID in the Rights Database" do
+      describe "#verify_rights_file" do
         it "logs no error" do
-          with_fake_rights_entries do
-            expect_ok(:verify_rights_file, test_rights_file_contents)
+          expect_ok(:verify_rights_file, test_rights_file_contents)
+        end
+      end
+    end
+
+    context "with no HTIDs in the rights database" do
+      around(:each) do |example|
+        Services[:database][:rights_current].truncate
+
+        example.run
+
+        Services[:database][:rights_current].truncate
+      end
+
+      describe "#run_for_date" do
+        it "logs `missing rights_current` error for full file" do
+          date = Date.new(2024, 11, 30)
+          with_fake_rights_file(date: date, full: true) do
+            verifier.run_for_date(date: date)
+            expect(verifier.errors).to include(/missing rights_current/)
+          end
+        end
+
+        it "logs `missing rights_current` error for update file" do
+          date = Date.new(2024, 12, 2)
+          with_fake_rights_file(date: date) do
+            verifier.run_for_date(date: date)
+            expect(verifier.errors).to include(/missing rights_current/)
           end
         end
       end
 
-      context "with HTID not in the Rights Database" do
+      describe "#verify_rights_file" do
         it "logs `missing rights_current` error" do
           expect_not_ok(:verify_rights_file, test_rights_file_contents, errmsg: /missing rights_current/)
         end
